6dc60046a21893e4188b30347dec07d062cfc669
joaquinperin98 <115036294+joaquinperin98@users.noreply.github.com>
2022-10-05 19:56:45 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "Hola";
+const nuevoNum = "Holaaa";
 
 // Crea una variable booleana:
 const nuevoBool = null;
