59c4271b1abf893a12546907c24b2cca7c2c7e30
joaquinperin98 <115036294+joaquinperin98@users.noreply.github.com>
2022-10-06 09:59:59 -0300

    tercer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "Holaaa";
+const nuevoNum = "Holaaaa";
 
 // Crea una variable booleana:
 const nuevoBool = null;
