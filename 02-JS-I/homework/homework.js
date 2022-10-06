388cd017802fde93775ad1409398a6f7b3d9c6e3
joaquinperin98 <115036294+joaquinperin98@users.noreply.github.com>
2022-10-06 11:41:57 -0300

    trabajo 1...

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -8,16 +8,16 @@ const nuevaString = 'Hola';
 const nuevoNum = '1998';
 
 // Crea una variable booleana:
-const nuevoBool = 'kk';
+const nuevoBool = 'true';
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
