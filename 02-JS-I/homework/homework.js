798b3b6c791e5b3a737b094bffc2537754eb995e
joaquinperin98 <115036294+joaquinperin98@users.noreply.github.com>
2022-10-07 19:44:56 -0300

    proceso work

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,21 +60,33 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-     
+     if (x === Y) {
+      return true;
+     } else {
+      return false;
+     }
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (str1 === str2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (num < 90) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function mayorQueCincuenta(num) {
